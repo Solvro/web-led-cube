bbb5074d7d935b7373dcfa0d732783966b666f96
HETTski <tanski.hubo@gmail.com>
2024-07-09 18:35:01 +0200

    adding resizable code editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import './App.css';
 import CubeScene from './components/CubeScene';
 import CodeEditor from './components/CodeEditor';
-import {useState} from "react";
-
+import { useState } from "react";
 
 function App() {
-
-    const [code, setCode] = useState("")
+    const [code, setCode] = useState("");
     const [isError, setIsError] = useState(false);
-
+    const [editorWidth, setEditorWidth] = useState(600);
+    const minEditorWidth = 100;
 
     const handleExecuteCode = (newCode) => {
         setCode(newCode);
     };
 
+    const handleMouseDown = (event) => {
+        event.preventDefault(); 
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp); 
+    }
+
+    const handleMouseMove = (event) => {
+        setEditorWidth(prevWidth => Math.max(minEditorWidth,prevWidth - event.movementX));
+    };
+
+    const handleMouseUp = () => {
+        document.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+    };
+
     return (
         <div className="App">
-            <CubeScene code={code} setIsError={setIsError} />
-            <CodeEditor onExecute={handleExecuteCode} isError={isError} />
+            <div className="container">
+                <CubeScene code={code} setIsError={setIsError} />
+                <div className="code-editor" style={{ width: editorWidth }}>
+                    <div className="resizer" onMouseDown={handleMouseDown} />
+                    <CodeEditor onExecute={handleExecuteCode} isError={isError} />
+                </div>
+            </div>
         </div>
-  );
+    );
 }
 
 export default App;
