2db9a87d7d0e88c6b8867428cf08f8a11f566514
HETTski <tanski.hubo@gmail.com>
2024-08-04 13:38:47 +0200

    transformed to flex, added responsivness

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import './App.css';
 import CubeScene from './components/CubeScene';
 import CodeEditor from './components/CodeEditor';
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function App() {
     const [code, setCode] = useState("");
     const [isError, setIsError] = useState(false);
     const [editorWidth, setEditorWidth] = useState(600);
     const minEditorWidth = 200;
+    const previousMouseX = useRef(null);
 
     const handleExecuteCode = (newCode) => {
         setCode(newCode);
     };
 
     const handleMouseDown = (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
+        previousMouseX.current = event.clientX;
         document.addEventListener('mousemove', handleMouseMove);
-        document.addEventListener('mouseup', handleMouseUp); 
-    }
+        document.addEventListener('mouseup', handleMouseUp);
+    };
 
     const handleMouseMove = (event) => {
-        setEditorWidth(prevWidth => Math.max(minEditorWidth,prevWidth - event.movementX));
+        const deltaX = event.clientX - previousMouseX.current;
+        previousMouseX.current = event.clientX;
+        setEditorWidth(prevWidth => Math.max(minEditorWidth, prevWidth - deltaX));
     };
 
     const handleMouseUp = () => {
