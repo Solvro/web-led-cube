48a462e0b640a184ef05e562e270f74c78808074
stepkos <stepkos@wp.pl>
2024-09-09 01:11:58 +0200

    feat: Improve cube

--- src/components/CubeScene.js
+++ src/components/CubeScene.js
@@ -1,4 +1,3 @@
-// src/ThreeScene.js
 import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
@@ -7,134 +6,138 @@ import "./../App.css";
 const CubeScene = ({ code, setIsError, isEditorVisible }) => {
     const mountRef = useRef(null);
     
-
-    
     useEffect(() => {
         const currentMount = mountRef.current;
         if (!currentMount) return;
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      20,
-      currentMount.clientWidth / currentMount.clientHeight,
-      0.01,
-      1000
-    );
-    camera.position.z = 10;
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(
+          20,
+          currentMount.clientWidth / currentMount.clientHeight,
+          0.01,
+          1000
+        );
+        camera.position.z = 10;
 
-    const renderer = new THREE.WebGLRenderer({
-      antialias: true,
-      alpha: true,
-    });
+        const renderer = new THREE.WebGLRenderer({
+          antialias: true,
+          alpha: true,
+        });
 
-    renderer.setSize(currentMount.clientWidth, currentMount.clientHeight);
-    currentMount.appendChild(renderer.domElement);
+        renderer.setSize(currentMount.clientWidth, currentMount.clientHeight);
+        currentMount.appendChild(renderer.domElement);
 
-    //const colorYellow = new THREE.Color("hsl(40, 100%, 60%)");
-    const colorLight = new THREE.Color("hsl(40, 100%, 90%)");
+        const colorLight = new THREE.Color("hsl(40, 100%, 90%)");
 
-    // const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-    // const cubeMaterial = new THREE.MeshPhongMaterial({
-    //   color: colorYellow,
-    // });
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
-    scene.add(ambientLight);
-    // const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    const cube = new THREE.Object3D();
+        // const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+        // const cubeMaterial = new THREE.MeshPhongMaterial({
+        //   color: colorYellow,
+        // });
+        // const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-    const color1 = new THREE.Color(0xff0000); // Red
-    const color2 = new THREE.Color(0x00ff00); // Green
-    const color3 = new THREE.Color(0x0000ff); // Blue
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+        scene.add(ambientLight);
+        const cube = new THREE.Object3D();
 
-    const numCubes = 10; // number of little cubes
-    const smallCubeSize = 0.1; // little cube size
+        const color1 = new THREE.Color(0xff0000); // Red
+        const color2 = new THREE.Color(0x00ff00); // Green
+        const color3 = new THREE.Color(0x0000ff); // Blue
 
-    for (let x = 0; x < numCubes; x++) {
-      for (let y = 0; y < numCubes; y++) {
-        for (let z = 0; z < numCubes; z++) {
-          const geometry = new THREE.BoxGeometry(
-            smallCubeSize,
-            smallCubeSize,
-            smallCubeSize
-          );
+        const cubesInRow = 7;
+        const smallCubeSize = 0.01;
+        const cubes = [];
 
-          const normalizedX = x / (numCubes - 1);
-          const normalizedY = y / (numCubes - 1);
-          const normalizedZ = z / (numCubes - 1);
+        for (let x = 0; x < cubesInRow; x++) {
+            cubes[x] = [];
 
-          const averageRatio = (normalizedX + normalizedY + normalizedZ) / 3;
+            for (let y = 0; y < cubesInRow; y++) {
+                cubes[x][y] = [];
 
-          let color;
-          if (averageRatio < 0.5) {
-            const ratio = averageRatio * 2; // Scale to [0, 1]
-            color = color1.clone().lerp(color2, ratio);
-          } else {
-            const ratio = (averageRatio - 0.5) * 2; // Scale to [0, 1]
-            color = color2.clone().lerp(color3, ratio);
-          }
+                for (let z = 0; z < cubesInRow; z++) {
 
-          const material = new THREE.MeshPhongMaterial({
-            color: color,
-            emissive: color,
-            emissiveIntensity: 2,
-          });
+                    const geometry = new THREE.BoxGeometry(
+                        smallCubeSize, smallCubeSize, smallCubeSize
+                    );
 
-          const smallCube = new THREE.Mesh(geometry, material);
+                    const normalizedX = x / (cubesInRow - 1);
+                    const normalizedY = y / (cubesInRow - 1);
+                    const normalizedZ = z / (cubesInRow - 1);
 
-          smallCube.position.set(
-            x * 0.1 - 1 / 2 + 0.1 / 2,
-            y * 0.1 - 1 / 2 + 0.1 / 2,
-            z * 0.1 - 1 / 2 + 0.1 / 2
-          );
+                    const averageRatio = (normalizedX + normalizedY + normalizedZ) / 3;
 
-          cube.add(smallCube);
-        }
-      }
-    }
+                    let color;
+                    if (averageRatio < 0.5) {
+                        const ratio = averageRatio * 2; // Scale to [0, 1]
+                        color = color1.clone().lerp(color2, ratio);
+                    }
+                    else {
+                        const ratio = (averageRatio - 0.5) * 2; // Scale to [0, 1]
+                        color = color2.clone().lerp(color3, ratio);
+                    }
 
-    const light = new THREE.PointLight(colorLight, 300, 1000);
-    const light2 = new THREE.PointLight(colorLight, 20, 1000);
+                    const material = new THREE.MeshPhongMaterial({
+                        color: color,
+                        emissive: color,
+                        emissiveIntensity: 2,
+                    });
 
-    light.position.set(-10, -10, 20);
-    light2.position.set(10, 20, 10);
+                    cubes[x][y][z] = new THREE.Mesh(geometry, material);
 
-    scene.add(light);
-    scene.add(light2);
-    scene.add(cube);
+                    cubes[x][y][z].position.set(
+                        x * 0.1 - 1 / 2 + 0.1 / 2,
+                        y * 0.1 - 1 / 2 + 0.1 / 2,
+                        z * 0.1 - 1 / 2 + 0.1 / 2
+                    );
 
-    const controls = new OrbitControls(camera, renderer.domElement);
-
-    const animate = () => {
-      try {
-        /* eslint-disable no-eval */
-        eval(code);
-        setIsError(false);
-      } catch (error) {
-        console.error("Error executing code: ", error);
-        setIsError(true);
-      }
-      // cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-      controls.update();
-      requestAnimationFrame(animate);
-    };
-
-    const handleResize = () => {
-      camera.aspect = currentMount.clientWidth / currentMount.clientHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(currentMount.clientWidth, currentMount.clientHeight);
-    };
-
-    animate();
-    window.addEventListener("resize", handleResize);
-
-        // Cleanup przy odmontowaniu komponentu
-        return () => {
-            window.removeEventListener('resize', handleResize);
-            if (currentMount) {
-                currentMount.removeChild(renderer.domElement);
+                    cube.add(cubes[x][y][z]);
+                }
             }
+        }
+
+        const light = new THREE.PointLight(colorLight, 300, 1000);
+        const light2 = new THREE.PointLight(colorLight, 20, 1000);
+
+        light.position.set(-10, -10, 20);
+        light2.position.set(10, 20, 10);
+
+        scene.add(light);
+        scene.add(light2);
+        scene.add(cube);
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+
+        const animate = () => {
+          try {
+            /* eslint-disable no-eval */
+            eval(code);
+            setIsError(false);
+          } catch (error) {
+            console.error("Error executing code: ", error);
+            setIsError(true);
+          }
+          // cube.rotation.y += 0.01;
+          renderer.render(scene, camera);
+          controls.update();
+          requestAnimationFrame(animate);
         };
+
+        const handleResize = () => {
+          camera.aspect = currentMount.clientWidth / currentMount.clientHeight;
+          camera.updateProjectionMatrix();
+          renderer.setSize(currentMount.clientWidth, currentMount.clientHeight);
+        };
+
+        animate();
+        window.addEventListener("resize", handleResize);
+
+            // Cleanup while unmount component
+            return () => {
+                window.removeEventListener('resize', handleResize);
+                if (currentMount) {
+                    currentMount.removeChild(renderer.domElement);
+                }
+            };
+
     }, [code, setIsError, isEditorVisible]);
 
   return <div className="cube-scene" ref={mountRef}></div>;
