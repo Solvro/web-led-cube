968ae2fe44eec6851f5d5bb359ec027dee7bdbf7
stepkos <stepkos@wp.pl>
2024-10-15 00:32:16 +0200

    feat: Disable React.StrictMode on production

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+if (process.env.NODE_ENV === 'production') {
+    root.render(
+        <App />
+    );
+}
+else {
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
