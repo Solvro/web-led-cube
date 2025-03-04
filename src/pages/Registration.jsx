37c50cdcd91a5ac82dfd1ba810d95efae7063bab
HETTski <tanski.hubo@gmail.com>
2025-03-04 12:21:45 +0100

    fix/registration completed but can be better

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -48,31 +48,32 @@ const Registration = () => {
 
   useEffect(() => {
     const result = USER_REGEX.test(user);
-    console.log("User " + user + " + " + result);
+console.log("User " + user + " + " + result);
+console.log("User " + user + " + " + result);
     setValidUser(result);
   }, [user]);
   useEffect(() => {
     const result = NAME_REGEX.test(name);
-    console.log("Name " + name + " + " + result);
+console.log("Name " + name + " + " + result);
     setValidName(result);
   }, [name]);
   useEffect(() => {
     const result = LNAME_REGEX.test(lname);
-    console.log("Lname " + lname + " + " + result);
+console.log("Lname " + lname + " + " + result);
     setValidLname(result);
   }, [lname]);
   useEffect(() => {
     const result = EMAIL_REGEX.test(email);
-    console.log("Email " + email + " + " + result);
+console.log("Email " + email + " + " + result);
     setValidEmail(result);
   }, [email]);
 
   useEffect(() => {
     const result = PWD_REGEX.test(pwd);
-    console.log("Pwd: " + pwd + " + " + result);
+console.log("Pwd: " + pwd + " + " + result);
     setValidPwd(result);
     const match = pwd === matchPwd;
-    console.log("Match: " + match);
+console.log("Match: " + match);
     setValidMatch(match);
   }, [pwd, matchPwd]);
 
@@ -96,7 +97,7 @@ const Registration = () => {
           headers: { "Content-Type": "application/json" },
         }
       );
-      // toast.success(JSON.stringify(response.data));
+// toast.success(JSON.stringify(response.data));
       console.log(JSON.stringify(response.data))
       toast.success("Registration complete");
       setUser("");
@@ -110,7 +111,7 @@ const Registration = () => {
       if (!err?.response) {
         toast.error("No Server Response");
       } else if (err.response?.status === 400) {
-        // Check if the error is due to email conflict
+// Check if the error is due to email conflict
         if (err.response?.data?.email) {
           toast.error("Email Already Taken");
         } else if (err.response?.data?.username) {
@@ -126,12 +127,12 @@ const Registration = () => {
 
   return (
     <div className="page-container w-full h-screen flex items-center justify-center bg-cover bg-center">
-      <section className="registration-section  text-white">
-        <h1>REGISTER</h1>
-        <img src={solvroLogo} alt="Solvro Logo" className="solvroImg w-7/10" />
-        <form onSubmit={handleSubmit} className="w-full flex flex-col items-center px-12 py-5">
-          <div className="label-input-section flex flex-col mb-10 w-full relative">
-            <label htmlFor="userName"  className="mb-1 h-8 flex items-center">
+      <section className="registration-section flex flex-col items-center justify-center w-1/3 h-2/3 text-white">
+        <h1 className="mb-2">REGISTER</h1>
+        <img src={solvroLogo} alt="Solvro Logo" className="solvroImg w-7/10 mb-2" />
+        <form onSubmit={handleSubmit} className="w-full flex flex-col items-center px-8 py-4">
+          <div className="label-input-section flex flex-col mb-4 w-full relative">
+            <label htmlFor="userName" className="mb-1 h-8 flex items-center">
               USERNAME
               <span className={validUser ? "valid" : "hide"}>
                 <IoCheckmarkOutline />
@@ -143,7 +144,7 @@ const Registration = () => {
             <input
               type="text"
               id="username"
-              className="w-full h-14 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none focus:border-black"
+              className="w-full h-12 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none focus:border-black"
               ref={userRef}
               autoComplete="username"
               onChange={(e) => setUser(e.target.value)}
@@ -159,15 +160,15 @@ const Registration = () => {
             <p
               id="uidnote"
               className={` tooltip
-            ${userFocus && user && !validUser ? "" : "offscreen"}
-            `}
+${userFocus && user && !validUser ? "" : "offscreen"}
+`}
             >
               4 to 24 characters. <br />
               Must begin with a letter. <br />
               Letters, numbers, underscores, hyphens allowed.
             </p>
           </div>
-          <div className="label-input-section flex flex-col w-full relative">
+          <div className="label-input-section flex flex-col mb-2 w-full relative">
             <label htmlFor="email">
               EMAIL
               <span className={validEmail ? "valid" : "hide"}>
@@ -180,7 +181,7 @@ const Registration = () => {
             <input
               type="text"
               id="email"
-              className="w-full h-14 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none focus:border-black"
+              className="w-full h-12 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none focus:border-black"
               ref={userRef}
               autoComplete="email"
               onChange={(e) => setEmail(e.target.value)}
@@ -191,79 +192,85 @@ const Registration = () => {
               placeholder="Enter your email"
             />
           </div>
-          <div className="label-input-section flex flex-col w-full relative">
-            <label htmlFor="name">
-              FIRST NAME
-              <span className={validName ? "valid" : "hide"}>
-                <IoCheckmarkOutline />
-              </span>
-              <span className={validName || !name ? "hide" : "invalid"}>
-                <IoCloseOutline />
-              </span>
-            </label>
-            <input
-              type="text"
-              id="name"
-              ref={userRef}
-              autoComplete="firstName"
-              className="w-full h-14 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none focus:border-black"
-              onChange={(e) => setName(e.target.value)}
-              required
-              value={name}
-              aria-invalid={validName ? "false" : "true"}
-              aria-describedby="uidnote"
-              onFocus={() => setNameFocus(true)}
-              onBlur={() => setNameFocus(false)}
-              placeholder="Enter your first name"
-            />
-            <p
-              id="uidnote"
-              className={` tooltip
-            ${nameFocus && name && !validName ? "" : "offscreen"}
-            `}
-            >
-              Cannot contain numbers. <br />
-              At least 1 letter long. <br />
-              Hyphens allowed.
-            </p>
+          <div className="flex flex-row-custom w-full space-x-4 mb-8">
+            <div className="label-input-section flex flex-col w-1/2 relative">
+              <label htmlFor="name">
+                FIRST NAME
+                <span className={validName ? "valid" : "hide"}>
+                  <IoCheckmarkOutline />
+                </span>
+                <span className={validName || !name ? "hide" : "invalid"}>
+                  <IoCloseOutline />
+                </span>
+              </label>
+              <input
+                type="text"
+                id="name"
+                ref={userRef}
+                autoComplete="firstName"
+                className="w-full h-12 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none focus:border-black"
+                onChange={(e) => setName(e.target.value)}
+                required
+                value={name}
+                aria-invalid={validName ? "false" : "true"}
+                aria-describedby="uidnote"
+                onFocus={() => setNameFocus(true)}
+                onBlur={() => setNameFocus(false)}
+                placeholder="Enter your first name"
+              />
+              <p
+
+                id="uidnote"
+                className={
+` tooltip
+${nameFocus && name && !validName ? "" : "offscreen"}
+`}
+              >
+                Cannot contain numbers. <br />
+                At least 1 letter long. <br />
+                Hyphens allowed.
+              </p>
+            </div>
+            <div className="label-input-section flex flex-col w-1/2 relative">
+              <label htmlFor="lname">
+                LAST NAME
+                <span className={validLname ? "valid" : "hide"}>
+                  <IoCheckmarkOutline />
+                </span>
+                <span className={validLname || !lname ? "hide" : "invalid"}>
+                  <IoCloseOutline />
+                </span>
+              </label>
+              <input
+                type="text"
+                id="lname"
+                className="w-full h-12 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none focus:border-black"
+                ref={userRef}
+                autoComplete="firstName"
+                onChange={(e) => setLname(e.target.value)}
+                required
+                value={lname}
+                aria-invalid={validLname ? "false" : "true"}
+                aria-describedby="uidnote"
+                onFocus={() => setLnameFocus(true)}
+                onBlur={() => setLnameFocus(false)}
+                placeholder="Enter your last name"
+
+              />
+              <p
+                id="uidnote"
+
+                className={` tooltip
+${lnameFocus && lname && !validLname ? "" : "offscreen"}
+`}
+              >
+                Cannot contain numbers. <br />
+                At least 1 letter long. <br />
+                Hyphens allowed.
+              </p>
+            </div>
           </div>
-          <div className="label-input-section">
-            <label htmlFor="lname">
-              LAST NAME
-              <span className={validLname ? "valid" : "hide"}>
-                <IoCheckmarkOutline />
-              </span>
-              <span className={validLname || !lname ? "hide" : "invalid"}>
-                <IoCloseOutline />
-              </span>
-            </label>
-            <input
-              type="text"
-              id="lname"
-              className="w-full h-14 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none focus:border-black"
-              ref={userRef}
-              autoComplete="firstName"
-              onChange={(e) => setLname(e.target.value)}
-              required
-              value={lname}
-              aria-invalid={validLname ? "false" : "true"}
-              aria-describedby="uidnote"
-              onFocus={() => setLnameFocus(true)}
-              onBlur={() => setLnameFocus(false)}
-              placeholder="Enter your last name"
-            />
-            <p
-              id="uidnote"
-              className={` tooltip
-            ${lnameFocus && lname && !validLname ? "" : "offscreen"}
-            `}
-            >
-              Cannot contain numbers. <br />
-              At least 1 letter long. <br />
-              Hyphens allowed.
-            </p>
-          </div>
-          <div className="label-input-section">
+          <div className="label-input-section flex flex-col mb-6 w-full relative">
             <label htmlFor="password">
               PASSWORD
               <span className={validPwd ? "valid" : "hide"}>
@@ -276,7 +283,7 @@ const Registration = () => {
             <input
               type="password"
               id="password"
-              className="w-full h-14 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none focus:border-black"
+              className="w-full h-12 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none focus:border-black"
               autoComplete="new-password"
               onChange={(e) => setPwd(e.target.value)}
               required
@@ -285,13 +292,15 @@ const Registration = () => {
               aria-describedby="pwdnote"
               onFocus={() => setPwdFocus(true)}
               onBlur={() => setPwdFocus(false)}
-              placeholder="Enter your password"
+              placeholder="
+Enter your password"
             />
             <p
+
               id="pwdnote"
               className={`tooltip ${
-                pwdFocus && !validPwd ? "instructions" : "offscreen"
-              }`}
+pwdFocus && !validPwd ? "instructions" : "offscreen"
+}`}
             >
               8 to 24 characters. <br />
               Must include uppercase and lowercase letters, a number and a
@@ -305,7 +314,7 @@ const Registration = () => {
               <span aria-label="percent">%</span>
             </p>
           </div>
-          <div className="label-input-section">
+          <div className="label-input-section flex flex-col mb-6 w-full relative">
             <label htmlFor="confirm_pwd">
               CONFIRM PASSWORD
               <span className={validMatch && matchPwd ? "valid" : "hide"}>
@@ -318,36 +327,39 @@ const Registration = () => {
             <input
               type="password"
               id="confirm_pwd"
-              className="w-full h-14 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none focus:border-black"
+              className="w-full h-12 rounded-md text-lg p-2 text-black bg-[#808796] border-3 border-transparent focus:outline-none f// I am not sure if I can give matchPwd value - if it causes problems, clear it
+ocus:border-black"
               autoComplete="new-password"
               onChange={(e) => setMatchPwd(e.target.value)}
               required
-              // I am not sure if I can give matchPwd value - if it causes problems, clear it
+// I am not sure if I can give matchPwd value - if it causes problems, clear it
               value={matchPwd}
               aria-invalid={validMatch ? "false" : "true"}
               aria-describedby="confirmnote"
               onFocus={() => setMatchFocus(true)}
               onBlur={() => setMatchFocus(false)}
+
               placeholder="Enter your password again"
+
             />
             <p
               id="confirmnote"
               className={`tooltip ${
-                matchFocus && !validMatch ? "instructions" : "offscreen"
-              }`}
+matchFocus && !validMatch ? "instructions" : "offscreen"
+}`}
             >
               Must match the first password input field.
             </p>
           </div>
 
           <button
-            disabled={!validUser || !validEmail || !validName || !validLname || !validPwd || !validMatch ? true : false}
-            className="sign-in-button w-4/5 h-14 rounded-md text-xl bg-[#152959] hover:bg-[#061527] text-white transition-all duration-300 ease-in-out" 
+            disabled={!validUser || !validEmail || !validName || !validLname || !validPwd || !validMatch}
+            className="sign-in-button w-4/5 h-12 rounded-md text-xl bg-[#152959] hover:bg-[#061527] text-white transition-all duration-300 ease-in-out"
           >
             Sign Up
           </button>
         </form>
-        <div className="info-container">
+        <div className="info-container mt-4">
           <p className="info-text">
             <Link to="/login" aria-label="Already registered? Sign in!" className="no-underline">
               <div className="info-button">Already registered?</div>
