5022f31ed73e3848eff51c097ba641ff6e8ec31a
jantar <mikikubs@gmail.com>
2025-03-12 17:34:17 +0100

    fix: updated code to new version

--- examples/wave.js
+++ examples/wave.js
@@ -1,30 +1,30 @@
 let currentWavePosition = 0
 let waveHeight = 2
 let waveWidth = 3
-let baseLayer = Math.floor(cubes.length / 3)
+let baseLayer = Math.floor(leds.length / 3)
 
 const COLOR_DEFAULT = 0x202020
 const COLOR_WATER = 0x0005d2
 const COLOR_FOAM = 0xffffff
+const interval = 300
 
 function animateWave () {
-  for (let x = 0; x < cubes.length; x++) {
-    for (let y = 0; y < cubes.length; y++) {
-      for (let z = 0; z < cubes.length; z++) {
+  for (let x = 0; x < leds.length; x++) {
+    for (let y = 0; y < leds.length; y++) {
+      for (let z = 0; z < leds.length; z++) {
         setCubeColor(x, y, z)
       }
     }
   }
 
-  currentWavePosition = (currentWavePosition + 1) % cubes.length
-  setTimeout(animateWave, 200)
+  currentWavePosition = (currentWavePosition + 1) % leds.length
+  setTimeout(animateWave, interval)
 }
 
 function setCubeColor (x, y, z) {
-  const cube = cubes[z][y][x]
+  const cube = leds[z][y][x]
 
-  cube.material.color.set(COLOR_DEFAULT)
-  cube.material.emissive.set(COLOR_DEFAULT)
+  cube.color.set(COLOR_DEFAULT)
 
   if (y < baseLayer) {
     if (
@@ -33,13 +33,11 @@ function setCubeColor (x, y, z) {
       Math.abs(x - currentWavePosition) >= waveWidth / 2
     ) {
       let color = Math.random() > 0.5 ? COLOR_FOAM : COLOR_DEFAULT
-      cube.material.color.set(color)
-      cube.material.emissive.set(color)
+      cube.color.set(color)
       return
     }
 
-    cube.material.color.set(COLOR_WATER)
-    cube.material.emissive.set(COLOR_WATER)
+    cube.color.set(COLOR_WATER)
     return
   }
 
@@ -51,9 +49,7 @@ function setCubeColor (x, y, z) {
     let rand = Math.random()
     let color =
       rand > 0.85 ? COLOR_FOAM : rand > 0.3 ? COLOR_WATER : COLOR_DEFAULT
-    cube.material.color.set(color)
-    cube.material.emissive.set(color)
-    return
+    cube.color.set(color)
   }
 }
 
